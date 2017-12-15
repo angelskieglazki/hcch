de49bed809f542fc1ec498c4d2b4048bb4c44ff3
max <mpano91@gmail.com>
2017-12-15 14:48:55 +0300

    update

--- hcch.c
+++ hcch.c
@@ -9,59 +9,59 @@
 
 const char *get_user_name()
 {
-    uid_t uid = geteuid();
-    struct passwd *pw = getpwuid(uid);
-    if (pw) {
-        return pw->pw_name;
-    }
-    return "";
+	uid_t uid = geteuid();
+	struct passwd *pw = getpwuid(uid);
+	if (pw) {
+		return pw->pw_name;
+	}
+	return "";
 }
 
 char *get_time()
 {
-    time_t t = time(NULL);
-    struct tm *tm = localtime(&t);
-    return asctime(tm);
+	time_t t = time(NULL);
+	struct tm *tm = localtime(&t);
+	return asctime(tm);
 }
 
-int main(int argc, char* argv[]) 
+int main(int argc, char *argv[])
 {
-    char *name;
-    if (argc == 2) {
-        name = malloc(strlen(argv[1]));
-        strncpy(name, argv[1], strlen(argv[1]));
-        char name_h[50];
-        char name_c[50];
-        snprintf(name_h, 100, "%s.h", name);
-        snprintf(name_c, 100, "%s.c", name);
+	char *name;
+	if (argc == 2) {
+		name = malloc(strlen(argv[1]));
+		strncpy(name, argv[1], strlen(argv[1]));
+		char name_h[50];
+		char name_c[50];
+		snprintf(name_h, 100, "%s.h", name);
+		snprintf(name_c, 100, "%s.c", name);
 
-        int i;
-        for (i =0; i<strlen(name); i++)
-            name[i] = toupper(name[i]);
-        char source[200];
-        int cx;
-        cx = snprintf(source, 200, SOURCE, name_h, get_user_name(), get_time(), name, name);
-        
-        int fd;
-        
-        if ((fd = open(name_h, O_RDWR|O_CREAT, 0644))<0) {
-            perror("Error to open create file");
-            exit(1);
-        }
-        write(fd, (const void*)source, strlen(source));
-        close(fd);
-        if ((fd = open(name_c, O_RDWR|O_CREAT, 0644))<0) {
-            perror("Error to open create file");
-            exit(1);
-        }
-        close(fd);
+		int i;
+		for (i = 0; i < strlen(name); i++)
+			name[i] = toupper(name[i]);
+		char source[200];
+		int cx;
+		cx = snprintf(source, 200, SOURCE, name_h, get_user_name(),
+			      get_time(), name, name);
 
+		int fd;
+
+		if ((fd = open(name_h, O_RDWR | O_CREAT, 0644)) < 0) {
+			perror("Error to open create file");
+			exit(1);
+		}
+		write(fd, (const void *)source, strlen(source));
+		close(fd);
+		if ((fd = open(name_c, O_RDWR | O_CREAT, 0644)) < 0) {
+			perror("Error to open create file");
+			exit(1);
+		}
+		close(fd);
 
 //        printf("%s", source);
-    } else {
-        perror("CMD LINE ARGS ERROR!!!\n");
-        exit(1);
-    }
-    
-    return 0;   
+	} else {
+		perror("CMD LINE ARGS ERROR!!!\n");
+		exit(1);
+	}
+
+	return 0;
 }
